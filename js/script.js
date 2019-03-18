39257ab8658849192b3a264e1dec11331c144c11
Lanai <lanai.lewis@gmail.com>
2019-03-17 19:49:16 -0700

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/******************************************
+Treehouse Techdegree:
+FSJS project 2 - List Filter and Pagination
+******************************************/
+   
+// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+
+
+/* 
+  Shown below are global variables, to be used throughout this program, but the following functions. 
+*/
+
+const elementsPerPage = 10;
+let currentPage = 1;
+const studentContainer = document.getElementById("student-container");
+const students = studentContainer.getElementsByTagName("li");
+
+// here is a loop to create a new student list
+
+var studentElements = [];
+for (i = 0; i < students.length; i += 1) {
+	studentElements.push(students[i]);
+}
+
+/*** 
+   Create the `showPage` function to hide all of the items in the 
+   list except for the ten you want to show.
+
+   Pro Tips: 
+     - Keep in mind that with a list of 54 students, the last page 
+       will only display four.
+     - Remember that the first student has an index of 0.
+     - Remember that a function `parameter` goes in the parens when 
+       you initially define the function, and it acts as a variable 
+       or a placeholder to represent the actual function `argument` 
+       that will be passed into the parens later when you call or 
+       "invoke" the function 
+***/
+
+
+
+
+/*** 
+   Create the `appendPageLinks function` to generate, append, and add 
+   functionality to the pagination buttons.
+***/
+
+
+
+
+
+// Remember to delete the comments that came with this file, and replace them with your own code comments.
