838fa109eff4ed17b420d041f692c515acf4eb3e
Lanai <lanai.lewis@gmail.com>
2019-03-20 19:36:52 -0700

    Add showPage function, and hide all students

--- js/script.js
+++ js/script.js
@@ -11,33 +11,28 @@ FSJS project 2 - List Filter and Pagination
 */
 
 const studentList = document.getElementsByTagName('li');
-const elementsPerPage = 10;
+let currentPage = 1;
 
-/*
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
 
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-*/
+// this is a function that hides all the students in the list
 
-/*
-while (studentList.hasChildNodes()) {
-      studentList.removeChild(studentList.firstChild);
-   }
-*/
+for(i = 0; i < studentList.length; i++) {
+	studentList[i].style.display = 'none';
+}
 
-function showPage() {
+
+
+function showPage(currentPage) {
 	for(let i = 0; i < studentList.length; i += 1) {
-		studentList[i].style.display = 'none';
+		const min = currentPage * 10 - 10;
+		const max = currentPage * 10 - 1;
+		if(i > min && i < max) {
+			// change display of selected students to default
+			studentList[i].style.display = '';
+		}
 	}
+	
+	
 }
 showPage();
 
